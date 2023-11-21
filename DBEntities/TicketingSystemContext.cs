ae5c742b71f1595b18db312bc65727d1d81e0d56
Shaymaa AlKhader <87859824+Shaima-AlKhader@users.noreply.github.com>
2023-11-21 19:06:56 +0300

    Update TicketingSystemContext.cs

--- DBEntities/TicketingSystemContext.cs
+++ DBEntities/TicketingSystemContext.cs
@@ -28,7 +28,7 @@ namespace TicketingSystem.DBEntities
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-BEB56J5;Database=TicketingSystem;Trusted_Connection=True; User Id=sa; password=12345; Integrated security=False;");
+                optionsBuilder.UseSqlServer("Server=[your machine name];Database=TicketingSystem;Trusted_Connection=True; User Id=[]; password=[]; Integrated security=False;");
             }
         }
 
